74b1fc921dd965284342ecb4fa16d9952b05cb78
nakayama.nobuo <nakayama.nobuo@nichiwa-system.co.jp>
2015-06-02 09:10:47 +0900

    テスト用

--- app/src/main/java/jp/co/nichiwa_system/yamashitamasaki/Sonaechao/popup.java
+++ app/src/main/java/jp/co/nichiwa_system/yamashitamasaki/Sonaechao/popup.java
@@ -13,14 +13,15 @@ public class popup extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_popup);
+        setContentView(R.layout.activity_popup);//テスト用コメント
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_popup, menu);
+        getMenuInflater().inflate(R.menu.menu_popup, menu);//テスト用コメント
+        
         return true;
     }
 
